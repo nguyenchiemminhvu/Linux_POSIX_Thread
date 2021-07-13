d2aafe5fe9f963d73eb5921fde3017c8bc417eef
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-07-13 20:28:12 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
 
+#define LOG(s) printf("%s\n", (s))
+
+void * PthreadSaysHello(void * arg)
+{
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    LOG("Pthread says hello");
+
+    pthread_t t;
+    pthread_create(&t, NULL, PthreadSaysHello, NULL);
+
+    return 0;
+}
