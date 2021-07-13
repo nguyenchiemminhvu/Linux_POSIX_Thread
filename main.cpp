1a632992ca9d54a67409dcf2df81e6bc717decb1
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-07-13 20:45:23 +0700

    update

--- main.cpp
+++ main.cpp
@@ -3,19 +3,26 @@
 #include <stdio.h>
 
 #define LOG(s) printf("%s\n", (s))
+#define ECHO(n) printf("%d\n", (n));
 
-void * PthreadSaysHello(void * arg)
+void * FetchData(void * arg)
 {
-    pthread_exit(NULL);
+    int &n = *(int*)arg;
+    n = 100;
+    pthread_exit((void*)0);
 }
 
 int main()
 {
     LOG("Pthread says hello");
 
+    int data = 0;
+
     pthread_t t;
-    pthread_create(&t, NULL, PthreadSaysHello, NULL);
+    pthread_create(&t, NULL, FetchData, (void*)&data);
     pthread_join(t, NULL);
+
+    ECHO(data);
 
     return 0;
 }
