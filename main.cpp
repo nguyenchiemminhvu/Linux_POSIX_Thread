08628b6508e07071d1a0f8c1760190edf6c0c562
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-07-13 22:14:18 +0700

    update

--- main.cpp
+++ main.cpp
@@ -5,10 +5,18 @@
 #define LOG(s) printf("%s\n", (s))
 #define ECHO(n) printf("%d\n", (n));
 
-void * FetchData(void * arg)
+int global_data = 0;
+pthread_once_t once_control = PTHREAD_ONCE_INIT;
+
+void InitOnce()
 {
-    int &n = *(int*)arg;
-    n = 100;
+    global_data++;
+}
+
+void * thread_func(void * arg)
+{
+    pthread_t temp;
+    pthread_once(&once_control, InitOnce);
     pthread_exit((void*)0);
 }
 
@@ -16,13 +24,18 @@ int main()
 {
     LOG("Pthread says hello");
 
-    int data = 0;
+    pthread_t t[3];
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_create(&t[i], NULL, thread_func, (void*)&t[i]);
+    }
 
-    pthread_t t;
-    pthread_create(&t, NULL, FetchData, (void*)&data);
-    pthread_join(t, NULL);
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_join(t[i], NULL);
+    }
 
-    ECHO(data);
+    ECHO(global_data);
 
     return 0;
 }
